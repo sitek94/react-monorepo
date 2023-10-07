7d48444e6c35b029511fb95d93acd9ea90ad24ae
Maciek Sitkowski <msitkowski94@gmail.com>
2023-10-07 15:11:50 +0200

    use tailwind to style ui library

--- libs/ui/src/lib/title.tsx
+++ libs/ui/src/lib/title.tsx
@@ -1,11 +1,9 @@
-import styles from './title.module.css';
-
 export type TitleProps = {
   children: React.ReactNode;
 };
 
 export function Title({ children }: TitleProps) {
-  return <h1 className={styles['container']}>{children}</h1>;
+  return <h1 className="text-4xl text-blue-500 font-bold">{children}</h1>;
 }
 
 export default Title;
